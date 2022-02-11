0b0c916d2779332bf515ae97205ad5fbeff9648f
Tony <mr.landero@gmail.com>
2022-02-11 13:36:24 -0600

    Begin the KaseiCoinCrowdsale contract by making it Crowdsale and MintedCrowdsale, and build the constructor function

--- KaseiCoinCrowdsale.sol
+++ KaseiCoinCrowdsale.sol
@@ -4,3 +4,9 @@ import "./KaseiCoinMintable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
 
+contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale {
+    constructor(
+        uint256 rate, // rate in TKNbits
+        address payable wallet, // sale beneficiary
+        KaseiCoin token // The KaseiCoin itselfthat our contract will work with
+    )
